18996895cf734240cfe58b49b7c0f543c9f42ce9
Corinne Lizzio <corinne.lizzio@gmail.com>
2014-07-03 17:43:00 -0400

    Added delete button

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,4 +1,5 @@
 class ArticleController < ApplicationController
+
   def index
     @articles = Article.all
   end
@@ -6,13 +7,22 @@ class ArticleController < ApplicationController
   def show
     @article =  Article.find(params[:id])
   end
+
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
     if @article.save!
       redirect_to @article
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy!
+      redirect_to action: :index
     end
   end
 
