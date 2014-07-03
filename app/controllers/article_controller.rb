4babf3f7e1ad25927dfe774834e318466bb50313
Corinne Lizzio <corinne.lizzio@gmail.com>
2014-07-03 14:25:21 -0400

    added new article form

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -2,8 +2,22 @@ class ArticleController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article =  Article.find(params[:id])
   end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    if @article.save!
+      redirect_to @article
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(:id, :title, :content)
+  end
+
 end
