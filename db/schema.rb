1e91e0fe69a1b016ebc749cb801903c4c920a3cb
Corinne Lizzio <corinne.lizzio@gmail.com>
2014-07-28 22:25:12 +0200

    Date Column

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140704181043) do
+ActiveRecord::Schema.define(version: 20140728201502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: true do |t|
-    t.string "title"
+    t.string  "title"
+    t.text    "content"
     t.integer "date"
-    t.text   "content"
   end
 
   create_table "images", force: true do |t|
